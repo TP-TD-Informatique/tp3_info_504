237b73f6fa0644555b44259605fcac80b10a2339
Kevin Traini <kevin@ktraini.com>
2020-10-21 13:51:15 +0200

    3.3

--- gtktetris.c
+++ gtktetris.c
@@ -13,11 +13,21 @@ int main(int argc, char *argv[]) {
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_widget_show(window);
 
+
+    GtkWidget *hbox1 = gtk_hbox_new(TRUE, 10);
+    // Add label
+    GtkWidget *label = gtk_label_new("Grille TETRIS");
+    gtk_container_add(GTK_CONTAINER (hbox1), label);
+    gtk_widget_show(label);
+
     GtkWidget *quitBtn;
     quitBtn = gtk_button_new_with_label("Quit");
     g_signal_connect(quitBtn, "clicked", G_CALLBACK(gtk_main_quit), NULL);
-    gtk_container_add(GTK_CONTAINER(window), quitBtn);
+    gtk_container_add(GTK_CONTAINER(hbox1), quitBtn);
     gtk_widget_show(quitBtn);
+
+    gtk_container_add(GTK_CONTAINER(window), hbox1);
+    gtk_widget_show(hbox1);
 
     gtk_main();
 
