205d6b102f9fae964bebcc987943ccb00fd754fb
Kevin Traini <kevin@ktraini.com>
2020-10-21 13:47:21 +0200

    3.1 + 3.2

---
+++ cmake-build-debug/CMakeFiles/tetris.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/kevin/DATA/Univ/semestre5/info504/tp3/gtktetris.c" "/home/kevin/DATA/Univ/semestre5/info504/tp3/cmake-build-debug/CMakeFiles/tetris.dir/gtktetris.c.o"
+  "/home/kevin/DATA/Univ/semestre5/info504/tp3/tetris.c" "/home/kevin/DATA/Univ/semestre5/info504/tp3/cmake-build-debug/CMakeFiles/tetris.dir/tetris.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/include/gtk-2.0"
+  "/usr/include/freetype2"
+  "/usr/include/glib-2.0"
+  "/usr/lib/x86_64-linux-gnu/glib-2.0/include"
+  "/usr/include/atk-1.0"
+  "/usr/include/gdk-pixbuf-2.0"
+  "/usr/include/cairo"
+  "/usr/include/harfbuzz"
+  "/usr/include/pango-1.0"
+  "/usr/lib/x86_64-linux-gnu/gtk-2.0/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- gtktetris.c
+++ gtktetris.c
@@ -6,10 +6,20 @@
 
 
 int main(int argc, char *argv[]) {
-    Grille g;
-    Piece tabPieces[NB_PIECES];
-    initialisePieces(tabPieces);
-    initialiseGrille(g);
-    afficheGrille(g);
-    return 0;
+    GtkWidget *window;
+
+    gtk_init(&argc, &argv);
+
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_widget_show(window);
+
+    GtkWidget *quitBtn;
+    quitBtn = gtk_button_new_with_label("Quit");
+    g_signal_connect(quitBtn, "clicked", G_CALLBACK(gtk_main_quit), NULL);
+    gtk_container_add(GTK_CONTAINER(window), quitBtn);
+    gtk_widget_show(quitBtn);
+
+    gtk_main();
+
+    return EXIT_SUCCESS;
 }
